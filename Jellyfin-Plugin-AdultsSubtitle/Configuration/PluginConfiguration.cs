0608b7240f4298d07aa0e88b973680e40ea6f677
fallingrust <55735959+fallingrust@users.noreply.github.com>
2024-03-24 00:10:57 +0800

    添加项目文件。

---
+++ Jellyfin-Plugin-AdultsSubtitle/Configuration/PluginConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin_Plugin_AdultsSubtitle.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+       
+    }
+}
