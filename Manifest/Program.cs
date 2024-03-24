85aa9039e88d01533d15c10b4ea25ecc891c0a32
fallingrust <55735959+fallingrust@users.noreply.github.com>
2024-03-24 15:39:54 +0800

    update

---
+++ Manifest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace Manifest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = args[0];
+            var md5 = args[1];
+            var path = args[2];
+            using var sr = new StreamReader(path);
+            var json = sr.ReadToEnd();
+            var packet = JsonSerializer.Deserialize(json, PackageInfoJsonContext.Default.IEnumerablePackageInfo)?.FirstOrDefault();
+
+            var versionInfo = packet!.Versions.FirstOrDefault(p => p.Version == version);
+            if (versionInfo == null)
+            {
+                packet!.Versions.Add(new VersionInfo()
+                {
+                    Checksum = md5,
+                    TargetAbi = "10.8.0.0",
+                    Changelog = $"update version {version}",
+                    Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    Version = version,
+                    SourceUrl = $"https://github.com/fallingrust/Jellyfin-Plugin-AdultsSubtitle/releases/download/v{version}/AdultsSubtitle.zip"
+                });
+            }
+            else
+            {
+                versionInfo.Checksum = md5;
+                versionInfo.Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            }
+            sr.Dispose();
+            using var sw = new StreamWriter(path);
+            sw.Write(JsonSerializer.Serialize(new PackageInfo[] { packet }, PackageInfoJsonContext.Default.IEnumerablePackageInfo));
+        }
+    }
+}
