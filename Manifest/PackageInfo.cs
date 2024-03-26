4b73643d74974705ab470311e1a71678057bfb7d
fallingrust <55735959+fallingrust@users.noreply.github.com>
2024-03-26 20:15:33 +0800

    .

---
+++ Manifest/PackageInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Manifest
+{
+    public class PackageInfo
+    {
+        public PackageInfo()
+        {
+            Versions = Array.Empty<VersionInfo>();
+            Category = string.Empty;
+            Name = string.Empty;
+            Overview = string.Empty;
+            Owner = string.Empty;
+            Description = string.Empty;
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; }
+
+        [JsonPropertyName("owner")]
+        public string Owner { get; set; }
+
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+
+        [JsonPropertyName("guid")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("versions")]
+
+        public IList<VersionInfo> Versions { get; set; }
+
+        [JsonPropertyName("imageUrl")]
+        public string? ImageUrl { get; set; }
+    }
+
+    [JsonSerializable(typeof(IEnumerable<PackageInfo>))]
+    [JsonSerializable(typeof(PackageInfo))]
+    public partial class PackageInfoJsonContext : JsonSerializerContext
+    {
+
+    }
+}
---
+++ Manifest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace Manifest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var localPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "manifest.json");
+            var version = args[0];
+            var sourceUrl = $"https://github.com/fallingrust/Jellyfin-Plugin-AdultsSubtitle/releases/download/{version}/AdultsSubtitle.zip";
+            using var client = new HttpClient();
+            var response = await client.GetAsync(sourceUrl);
+            var stream = await response.Content.ReadAsStreamAsync();
+            using var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            ms.Position = 0;
+
+
+            var md5 = GetMD5(ms);
+            
+            using var sr = new StreamReader(localPath);
+            var json = sr.ReadToEnd();
+            var packet = JsonSerializer.Deserialize(json, PackageInfoJsonContext.Default.IEnumerablePackageInfo)?.FirstOrDefault();
+
+            var versionInfo = packet!.Versions.FirstOrDefault(p => p.Version == version);
+            if (versionInfo == null)
+            {
+                packet!.Versions.Add(new VersionInfo()
+                {
+                    Checksum = md5,
+                    TargetAbi = "10.8.13",
+                    Changelog = $"update version {version}",
+                    Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    Version = version.Replace("v",""),
+                    SourceUrl = sourceUrl
+                });
+            }
+            else
+            {
+                versionInfo.Checksum = md5;
+                versionInfo.Timestamp = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            }
+            sr.Dispose();
+            using var sw = new StreamWriter(localPath);
+            sw.Write(JsonSerializer.Serialize(new PackageInfo[] { packet }, PackageInfoJsonContext.Default.IEnumerablePackageInfo));
+        }
+
+        private static string GetMD5(Stream ms)
+        {
+            using var md5 = MD5.Create();
+           
+            var buffer = md5.ComputeHash(ms);
+            return BitConverter.ToString(buffer).Replace("-", "").ToLower();
+        }
+    }
+}
