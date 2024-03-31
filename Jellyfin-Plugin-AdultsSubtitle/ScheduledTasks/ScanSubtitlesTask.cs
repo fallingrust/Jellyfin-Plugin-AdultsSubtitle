b53455e04c6f03eeeab2c5c4509de5cab44c37c6
fallingrust <55735959+fallingrust@users.noreply.github.com>
2024-03-31 16:18:01 +0800

    add update plugin task

---
+++ Jellyfin-Plugin-AdultsSubtitle/ScheduledTasks/ScanSubtitlesTask.cs
@@ -0,0 +1,54 @@
+﻿using Jellyfin.Data.Enums;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Jellyfin_Plugin_AdultsSubtitle.ScheduledTasks
+{
+    //public class ScanSubtitlesTask : IScheduledTask
+    //{
+    //    public string Name => "Scan Subtitles";
+
+    //    public string Key => $"{AdultsSubtitlePlugin.Instance.Name}ScanSubtitles";
+
+    //    public string Description => "Scan subtitles and download missing subtitles";
+
+    //    public string Category => AdultsSubtitlePlugin.Instance.Name;
+    //    private readonly ILibraryManager _libraryManager;
+    //    private readonly ILogger<ScanSubtitlesTask> _logger;
+    //    private readonly IProviderManager _providerManager;
+    //    public ScanSubtitlesTask(ILibraryManager libraryManager, IProviderManager providerManager, ILogger<ScanSubtitlesTask> logger)
+    //    {
+    //        _libraryManager = libraryManager;
+    //        _providerManager = providerManager;
+    //        _logger = logger;
+    //    }
+    //    public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+    //    {
+    //        var items = _libraryManager.GetItemList(new InternalItemsQuery()
+    //        {
+    //            MediaTypes = new[] { MediaType.Video },
+    //            SourceTypes = new[] { SourceType.Library },
+    //        });
+    //        foreach (var item in items)
+    //        {
+    //            var option = _libraryManager.GetLibraryOptions(item);
+    //        }
+           
+    //        _providerManager.GetAllMetadataPlugins();
+    //        return Task.CompletedTask;
+    //    }
+
+    //    public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+    //    {
+    //        yield return new TaskTriggerInfo
+    //        {
+    //            Type = TaskTriggerInfo.TriggerDaily,
+    //            TimeOfDayTicks = TimeSpan.FromHours(5).Ticks
+    //        };
+    //    }
+    //}
+}
---
+++ Jellyfin-Plugin-AdultsSubtitle/ScheduledTasks/UpdatePluginTask.cs
@@ -0,0 +1,97 @@
+﻿using AngleSharp.Dom;
+using MediaBrowser.Common;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.IO;
+using MediaBrowser.Model.Tasks;
+using MediaBrowser.Model.Updates;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Jellyfin_Plugin_AdultsSubtitle.ScheduledTasks
+{
+    public class UpdatePluginTask : IScheduledTask
+    {
+        public string Name => "Update Plugin";
+
+        public string Key => $"{AdultsSubtitlePlugin.Instance.Name}UpdatePlugin";
+
+        public string Description => $"Updates {AdultsSubtitlePlugin.Instance.Name} plugin to latest version.";
+
+        public string Category => AdultsSubtitlePlugin.Instance.Name;
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<UpdatePluginTask> _logger;
+        private readonly IPluginManager _pluginManager;
+        private readonly IApplicationPaths _applicationPaths;
+        private readonly IApplicationHost _applicationHost;
+        public UpdatePluginTask(IHttpClientFactory httpClientFactory, IPluginManager pluginManager, IApplicationPaths applicationPaths, IApplicationHost applicationHost, ILogger<UpdatePluginTask> logger)
+        {
+            _httpClientFactory = httpClientFactory;
+            _pluginManager = pluginManager;
+            _applicationPaths = applicationPaths;
+            _applicationHost = applicationHost;
+            _logger = logger;
+        }
+        public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var curVersion = Assembly.GetExecutingAssembly().GetName().Version;
+                var lastestVersion = await GetLatestVersionAsync(cancellationToken);
+                _logger.LogInformation($"Updates {AdultsSubtitlePlugin.Instance.Name} plugin to latest version:curVersion{curVersion} lastestVersion:{lastestVersion} ");
+                if (curVersion != null && curVersion.CompareTo(lastestVersion.Value.Item1) < 0)
+                {
+                    using var httpClient = _httpClientFactory.CreateClient();
+                    var response = await httpClient.GetAsync(lastestVersion.Value.Item2, cancellationToken).ConfigureAwait(false);
+                    var zipStream = await response.Content.ReadAsStreamAsync(cancellationToken);
+                    using var ms = new MemoryStream();
+                    await zipStream.CopyToAsync(ms, cancellationToken);
+                    ms.Position = 0;
+                    using var archive = new ZipArchive(ms);
+                    archive.ExtractToDirectory(Path.Combine(_applicationPaths.PluginsPath, $"AdultsSubtitle_{lastestVersion.Value.Item1}"), true);
+
+                    var curVersionPlugin = _pluginManager.GetPlugin(AdultsSubtitlePlugin.Instance.Id, curVersion);
+                    if (curVersionPlugin != null)
+                        _pluginManager.RemovePlugin(curVersionPlugin);
+                    _logger.LogInformation("remove cur version plugin");   
+                    _applicationHost.NotifyPendingRestart();
+                    _logger.LogInformation("update plugin complete");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.ToString());
+            }
+        }
+        public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
+        {
+            yield return new TaskTriggerInfo
+            {
+                Type = TaskTriggerInfo.TriggerDaily,
+                TimeOfDayTicks = TimeSpan.FromHours(5).Ticks
+            };
+        }
+
+      
+
+        private async Task<(Version,string)?> GetLatestVersionAsync(CancellationToken cancellationToken)
+        {
+            using var httpClient = _httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync("https://raw.githubusercontent.com/fallingrust/Jellyfin-Plugin-AdultsSubtitle/master/manifest.json", cancellationToken);
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            var packInfo = JsonSerializer.Deserialize<List<PackageInfo>>(content)?.FirstOrDefault();
+            if (packInfo != null)
+            {
+                var latestVersion = packInfo.Versions.OrderByDescending(p => p.VersionNumber).FirstOrDefault();
+                if (latestVersion != null && latestVersion.SourceUrl != null)
+                    return (latestVersion.VersionNumber, latestVersion.SourceUrl);
+            }
+            return null;
+        }
+    }
+}
